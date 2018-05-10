08885c19ac5af4bf45c005d15f1b2fbdb84ec272
clauderichard <claude.richard2@gmail.com>
2018-05-10 10:06:56 -0400

    Add files via upload

---
+++ Probzee.hs
@@ -0,0 +1,12 @@
+module Probzee
+where
+
+import Probzee.Dicing
+import Probzee.Slist
+import Probzee.Listutil
+import Probzee.Smatrix
+import Probzee.CoinStacks
+import Probzee.DynamicAlgorithm
+
+probzee = computeWinningProbability
+probzeeD = computeWinningProbabilityD
